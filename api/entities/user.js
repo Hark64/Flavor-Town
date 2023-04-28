7bab06d1c770ea55a7ee39dfc6b619f3664da4e8
spicytrampoline <103013493+spicytrampoline@users.noreply.github.com>
2023-04-27 20:47:43 -0700

    in class

--- api/entities/user.js
+++ api/entities/user.js
@@ -5,9 +5,15 @@ export default class User {
   @PrimaryGeneratedColumn()
   id
 
-  @Column({ type: 'varchar', unique: true })
+  @Column({ type: 'varchar', unique: true })  // Unique automatically includes nullable: false. 
   email
 
   @Column({ type: 'varchar', nullable: false })
   password
+
+  @Column({type: 'varchar', nullable: false })
+  firstName
+
+  @Column({type: 'varchar', nullable: false })
+  lastName
 }
