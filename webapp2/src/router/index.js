0b5c80c1cf38701f49445fc22ca87c20edf122bf
Matthew Kim <mattkim@Matthews-MacBook-Air-4.local>
2023-05-18 20:14:16 -0700

    updated index

--- webapp2/src/router/index.js
+++ webapp2/src/router/index.js
@@ -6,11 +6,11 @@ import AboutView from '../views/AboutView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
     {
       path: '/about',
       name: 'about',
@@ -19,13 +19,8 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    // {
-    //   path: '/todos',
-    //   name: 'todos',
-    //   component: () => import('../views/ToDosView.vue')
-    // },
     {
-      path: "/",
+      path: "/account",
       name: "account",
       component: () => import("../views/Account.vue")
     }
