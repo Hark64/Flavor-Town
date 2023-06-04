9dec5c2b1d14df954d866f88428c396573b90499
Anthony Lee <anthlee494@gmail.com>
2023-06-04 12:17:44 -0700

    deleted dead code

--- webapp2/src/main.js
+++ webapp2/src/main.js
@@ -26,31 +26,3 @@ app.use(vuetify)
 app.mount('#app')
 
 
-
-
-// import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-
-// // Vuetify
-// import 'vuetify/styles'
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-// import '@mdi/font/css/materialdesignicons.css'
-
-// const vuetify = createVuetify({
-//   components,
-//   directives,
-// });
-
-// import App from './App.vue'
-// import router from './router'
-
-// import './assets/main.css'
-
-// const app = createApp(App)
-
-// app.use(createPinia())
-// app.use(router)
-// app.use(vuetify)
-// app.mount('#app')
