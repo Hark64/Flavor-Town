3476371dae92b1fed11b826ade3d363436bb8aea
Matthew Kim <mattkn1012@gmail.com>
2023-06-09 16:40:22 -0700

    updated

--- webapp2/src/views/HomeView.vue
+++ webapp2/src/views/HomeView.vue
@@ -1,34 +1,34 @@
 <script setup>
-  import SearchBar from '../components/SearchBar.vue'
-  import Results from '../components/Querecipe.vue'
-  import { onMounted, ref } from 'vue';
-  import axios from 'axios'
-  import { useUserStore } from '@/stores/user';
+import SearchBar from '../components/SearchBar.vue'
+import Results from '../components/Querecipe.vue'
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+import { useUserStore } from '@/stores/user';
 
-  const store = useUserStore();
+const store = useUserStore();
 
-  const params = {};
-  let homeRecipes = ref([]);
+const params = {};
+let homeRecipes = ref([]);
 
-  params.count=10;
+params.count = 10;
 
 onMounted(() => {
-    if (store.loggedIn==true) {
-      store.getUser({}).then((error) => {
-        if(!error){
-          console.log(store.currentUser, store.hasError);
-          params.showZip = true;
-          params.zip = store.currentUser.zipCode.value;
-        }
-      });
-    }
+  if (store.loggedIn == true) {
+    store.getUser({}).then((error) => {
+      if (!error) {
+        console.log(store.currentUser, store.hasError);
+        params.showZip = true;
+        params.zip = store.currentUser.zipCode.value;
+      }
+    });
+  }
 
-    axios.get("/api/search", { params }).then((_recipes) => {
-        homeRecipes.value = _recipes.data.recipes;
-      },
-      (error) => {
-        console.error(error);
-      });
+  axios.get("/api/search", { params }).then((_recipes) => {
+    homeRecipes.value = _recipes.data.recipes;
+  },
+    (error) => {
+      console.error(error);
+    });
 
 });
 </script>
@@ -38,71 +38,178 @@ onMounted(() => {
 
 
   <div class="search">
-    <SearchBar/>
+    <SearchBar />
   </div>
 
   <div class="content">
     <h1 class="headers">For you</h1>
     <div class="posts-container">
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 1">
-      </div>
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 2">
-      </div>
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 3">
-      </div>
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 4">
-      </div>
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 5">
-      </div>
-      <div class="post">
-        <img src="../assets/post.png" alt="Post 6">
-      </div>
+      <v-container class="post">
+        <v-row justify="space-around">
+          <v-card width="260">
+            <v-img height="160" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/01/Orange-Chicken-square-1200.jpg" cover
+              class="text-white">
+            </v-img>
+            <v-card-text>
+              <div class="ms-1 mb-3">
+                30-40 min
+              </div>
+              <div class="font-weight-bold ms-1 mb-2">
+                Tangy Orange Chicken
+              </div>
+              <v-timeline density="compact">
+                <v-timeline-item v-for="message in messages" :key="message.time" :dot-color="message.color"
+                  size="x-small">
+                  <div class="mb-4">
+                    <div class="font-weight-normal">
+                      <strong>{{ message.from }}</strong> @{{ message.time }}
+                    </div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-container>
+      <v-container class="post">
+        <v-row justify="space-around">
+          <v-card width="260">
+            <v-img height="160" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/01/Orange-Chicken-square-1200.jpg" cover
+              class="text-white">
+            </v-img>
+            <v-card-text>
+              <div class="ms-1 mb-3">
+                30-40 min
+              </div>
+              <div class="font-weight-bold ms-1 mb-2">
+                Tangy Orange Chicken
+              </div>
+              <v-timeline density="compact">
+                <v-timeline-item v-for="message in messages" :key="message.time" :dot-color="message.color"
+                  size="x-small">
+                  <div class="mb-4">
+                    <div class="font-weight-normal">
+                      <strong>{{ message.from }}</strong> @{{ message.time }}
+                    </div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-container>
+      <v-container class="post">
+        <v-row justify="space-around">
+          <v-card width="260">
+            <v-img height="160" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/01/Orange-Chicken-square-1200.jpg" cover
+              class="text-white">
+            </v-img>
+            <v-card-text>
+              <div class="ms-1 mb-3">
+                30-40 min
+              </div>
+              <div class="font-weight-bold ms-1 mb-2">
+                Tangy Orange Chicken
+              </div>
+              <v-timeline density="compact">
+                <v-timeline-item v-for="message in messages" :key="message.time" :dot-color="message.color"
+                  size="x-small">
+                  <div class="mb-4">
+                    <div class="font-weight-normal">
+                      <strong>{{ message.from }}</strong> @{{ message.time }}
+                    </div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-container>
+      <v-container class="post">
+        <v-row justify="space-around">
+          <v-card width="260">
+            <v-img height="160" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/01/Orange-Chicken-square-1200.jpg" cover
+              class="text-white">
+            </v-img>
+            <v-card-text>
+              <div class="ms-1 mb-3">
+                30-40 min
+              </div>
+              <div class="font-weight-bold ms-1 mb-2">
+                Tangy Orange Chicken
+              </div>
+              <v-timeline density="compact">
+                <v-timeline-item v-for="message in messages" :key="message.time" :dot-color="message.color"
+                  size="x-small">
+                  <div class="mb-4">
+                    <div class="font-weight-normal">
+                      <strong>{{ message.from }}</strong> @{{ message.time }}
+                    </div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-container>
+      <v-container class="post">
+        <v-row justify="space-around">
+          <v-card width="260">
+            <v-img height="160" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/01/Orange-Chicken-square-1200.jpg" cover
+              class="text-white">
+            </v-img>
+            <v-card-text>
+              <div class="ms-1 mb-3">
+                30-40 min
+              </div>
+              <div class="font-weight-bold ms-1 mb-2">
+                Tangy Orange Chicken
+              </div>
+              <v-timeline density="compact">
+                <v-timeline-item v-for="message in messages" :key="message.time" :dot-color="message.color"
+                  size="x-small">
+                  <div class="mb-4">
+                    <div class="font-weight-normal">
+                      <strong>{{ message.from }}</strong> @{{ message.time }}
+                    </div>
+                    <div>{{ message.message }}</div>
+                  </div>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-container>
     </div>
   </div>
 
-    <div v-if="homeRecipes.length==0">
-      <h1>Looks Like No Recipes Are Available</h1>
+  <div v-if="homeRecipes.length == 0">
+    <h1>Looks Like No Recipes Are Available</h1>
+  </div>
+  <div v-else>
+    <div v-if="store.loggedIn == true">
+      <h1>
+        Recipes in Your Area (Based On ZipCode *DOESNT_WORK*)
+      </h1>
     </div>
     <div v-else>
-      <div v-if="store.loggedIn==true">
-        <h1>
-          Recipes in Your Area (Based On ZipCode *DOESNT_WORK*)
-        </h1>
-      </div>
-      <div v-else>
-        <h1>
-          Random Recipes
-        </h1>
-      </div>
-      
-      <v-sheet
-        class="mx-auto"
-        elevation="8"
-        max-width="800"
-      >
-        <v-slide-group
-          class="pa-4"
-          show-arrows
-        >
-          <v-slide-group-item
-            v-for="result in homeRecipes"
-            :key="result.id"
-          >
-            <Results
-              :obj="result"
-              :width="'300px'"
-              :height="'200px'"
-            ></Results>
-          </v-slide-group-item>
-        </v-slide-group>
-      </v-sheet>
+      <h1>
+        Random Recipes
+      </h1>
     </div>
-  </main>
+
+    <v-sheet class="mx-auto" elevation="8" max-width="800">
+      <v-slide-group class="pa-4" show-arrows>
+        <v-slide-group-item v-for="result in homeRecipes" :key="result.id">
+          <Results :obj="result" :width="'300px'" :height="'200px'"></Results>
+        </v-slide-group-item>
+      </v-slide-group>
+    </v-sheet>
+  </div>
 </template>
 
 <style scoped>
@@ -148,13 +255,7 @@ onMounted(() => {
 }
 
 .post {
-  width: 20%;
-  margin: 10px;
-  box-sizing: border-box;
-  background-color: #ffffff;
-  border-radius: 10px;
-  overflow: hidden;
-  box-sizing: border-box;
+  cursor: pointer;
 }
 
 .post img {
